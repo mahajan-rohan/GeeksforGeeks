3a5565963c261d895bcf0e3e25498a9d829a1919
wizard-1573 <rohan.m.150703@gmail.com>
2024-06-23 21:30:43 +0530

    added solutions

--- School/AutomorphicNumber.java
+++ School/AutomorphicNumber.java
@@ -1,6 +1,6 @@
 
 /*
-morphic Number
+Automorphic Number
 Given a number N, check whether the number is Automorphic number or not.
 A number is called Automorphic number if and only if its square ends in 
 has the same last digit as the number itself.
